d8ac81e696f5c1a20e4558fcacea9fb3fc024599
Yamany <Yamany@3ddx.com>
2017-09-24 17:03:42 +0200

    Add findUserByUsername function

--- routes/api.js
+++ routes/api.js
@@ -17,6 +17,14 @@ function Auth(req, res, next) {
 
 //router.use(Auth);
 
+var findUserByUsername = function (username, callback) {
+  models.User.findAll({where:{name: username}}).then(function(users) {
+    if(Object.keys(users).length < 1)
+      return callback(new Error('The user with username '+ username +' was not found!'));
+    return callback(null, users)
+  });
+}
+
 router.get('/', function(req, res) {
   var name = req.query.n;
   var account = {
@@ -28,9 +36,17 @@ router.get('/', function(req, res) {
 })
 
 router.get('/users', function(req, res) {
-  models.User.findAll({where:{id:{$gt: 1}}}).then(function(users) {
+  models.User.findAll({where:{id:{$gte: 1}}}).then(function(users) {
     res.json(users);
   });
+})
+
+router.get('/users/:username', function (req, res, next) {
+  var username = req.params.username;
+  findUserByUsername(username, function (err, users) {
+    if(err) return next(err);
+    return res.json(users);
+  })
 })
 
 module.exports = router;
